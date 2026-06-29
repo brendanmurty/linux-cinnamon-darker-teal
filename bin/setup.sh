3e500d2b2c18a57cb026671330cf7f40c968a3e9
Brendan Murty <brendan@bcm.works>
2026-06-29 10:32:21 +1000

    Setup script improvements and docs

--- bin/setup.sh
+++ bin/setup.sh
@@ -10,9 +10,26 @@ REPO="$(cd "$(dirname "$0")/.." && pwd)"
 THEME_CUSTOM="$REPO/theme"
 THEME_TARGET="/usr/share/themes/Mint-Y-Dark-Teal/cinnamon"
 
+echo 'Requesting sudo'
+sudo -v
+
 if [ -d "$THEME_TARGET" ]; then
-  echo "Backing up existing theme..."
-  mv "$THEME_TARGET" "$THEME_TARGET.default"
+  echo "Backing up existing theme directory to '$THEME_TARGET.default'"
+
+  sudo mv "$THEME_TARGET" "$THEME_TARGET.default"
 fi
 
-ln -s "$THEME_CUSTOM" "$THEME_TARGET"
+echo "Setting up a symlink - '$THEME_TARGET' > '$THEME_CUSTOM'"
+
+sudo ln -s "$THEME_CUSTOM" "$THEME_TARGET"
+sudo chown -R "$(id -u):$(id -g)" "$THEME_TARGET"
+
+echo "Updating system config to use the custom theme"
+
+gsettings set org.cinnamon.theme name 'Mint-Y-Dark-Teal'
+
+echo "Restarting Cinnamon to apply the custom theme"
+
+restart-cinnamon
+
+echo "Done"
